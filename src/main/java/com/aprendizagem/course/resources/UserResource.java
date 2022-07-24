5ae6a2bcd548b0232cc513bdaf709a0a6c5668fc
Matheus Quiles <matheusquiles@systemhaus.com.br>
2022-07-24 14:27:03 -0300

    User entity and resource

---
+++ src/main/java/com/aprendizagem/course/resources/UserResource.java
@@ -0,0 +1,20 @@
+package com.aprendizagem.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.aprendizagem.course.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		User u = new User(1l, "Matheus", "matheus@gmail.com", "999", "123456");
+		return ResponseEntity.ok().body(u);
+	}
+
+}
